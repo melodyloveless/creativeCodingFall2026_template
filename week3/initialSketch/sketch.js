e6e9c2d9d95225de4ce8c84352b58daebbe939af
melodyloveless <5409179+melodyloveless@users.noreply.github.com>
2026-09-21 17:10:28 -0400

    Updated template and main index.html page

---
+++ week3/initialSketch/sketch.js
@@ -0,0 +1,11 @@
+// For your initial sketch
+
+function setup() {
+  createCanvas(500, 500);
+}
+
+function draw() {
+  background(255,255,0);
+  text("Initial Sketch", 100, height/2);
+  // Insert your drawing here
+}
